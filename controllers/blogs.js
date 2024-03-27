42b81d8fe35d42d4c1907421ce1fde421377bc95
dermotbg <dermotbateman@gmail.com>
2024-03-27 08:47:29 +0200

    done: 13.5

---
+++ controllers/blogs.js
@@ -0,0 +1,33 @@
+const router = require('express').Router()
+
+const { Blog } = require('../models')
+
+router.get('/', async (req, resp) => {
+  const blogs = await Blog.findAll()
+  console.log(JSON.stringify(blogs, null, 2))
+  return resp.json(blogs)
+})
+
+router.post('/', async (req, resp) => {
+    try {
+      const blog = await Blog.create(req.body)
+      return resp.json(blog)
+    } catch (error) {
+      console.log('An error occurred:', error)
+      return resp.status(400)
+    }
+})
+
+router.delete('/:id', async (req, resp) => {
+  const blogToDelete = await Blog.findByPk(req.params.id)
+  console.log(blogToDelete)
+  if (blogToDelete){
+    await blogToDelete.destroy()
+    return resp.status(204).end()
+  }
+  else{
+    return resp.status(404).end()
+  }
+})
+
+module.exports = router
