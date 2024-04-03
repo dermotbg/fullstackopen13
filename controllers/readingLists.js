9d825f0f3843bc76a9667a45972bced06a98944a
dermotbg <dermotbateman@gmail.com>
2024-04-03 10:45:06 +0300

    done 13.22

--- controllers/readingLists.js
+++ controllers/readingLists.js
@@ -2,6 +2,17 @@ const { ReadingList } = require('../models')
 
 const router = require('express').Router()
 
+router.put('/:id', async (req, resp) => {
+  try {
+    const listEntry = await ReadingList.findByPk(req.params.id)
+    listEntry.read = req.body.read
+    listEntry.save()
+    return resp.status(204).json(listEntry)
+  } catch {
+    return resp.status(400).json({ error: error })
+  }
+})
+
 router.post('/', async (req, resp) => {
   try {
     const listEntry = await ReadingList.create({
