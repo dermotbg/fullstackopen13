60f69fefec05ed533ac32dc89965c7390dfa5c3f
dermotbg <dermotbateman@gmail.com>
2024-04-03 08:03:09 +0300

    done: 13.19

--- models/index.js
+++ models/index.js
@@ -1,13 +1,17 @@
 const { Blog } = require('./blog')
+const { ReadingList } = require('./readingList')
 const { User } = require('./user')
 
 
 User.hasMany(Blog)
 Blog.belongsTo(User)
+
+Blog.belongsToMany(User, { through: ReadingList })
 // Blog.sync({ alter: true })
 // User.sync({ alter: true })
 
 module.exports = {
   Blog,
-  User
+  User,
+  ReadingList
 }
