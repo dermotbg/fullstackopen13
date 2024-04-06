de43865f465ec6e1ab82f68680501f5db00232d9
dermotbg <dermotbateman@gmail.com>
2024-04-06 09:04:13 +0300

    feat: add session validation to reading lists

--- controllers/readingLists.js
+++ controllers/readingLists.js
@@ -1,27 +1,38 @@
 const { ReadingList } = require('../models')
+const { tokenExtractor, sessionValidator } = require('../util/middleware')
 
 const router = require('express').Router()
 
-router.put('/:id', async (req, resp) => {
-  try {
-    const listEntry = await ReadingList.findByPk(req.params.id)
-    listEntry.read = req.body.read
-    listEntry.save()
-    return resp.status(204).json(listEntry)
-  } catch {
-    return resp.status(400).json({ error: error })
+router.put('/:id', tokenExtractor, sessionValidator, async (req, resp) => {
+  if(req.sessionValid) {
+    try {
+      const listEntry = await ReadingList.findByPk(req.params.id)
+      listEntry.read = req.body.read
+      listEntry.save()
+      return resp.status(204).json(listEntry)
+    } catch {
+      return resp.status(400).json({ error: 'Malformed Request' })
+    }
+  }
+  else {
+    return resp.status(401).json ({ error: 'Please log in to update your reading list' })
   }
 })
 
-router.post('/', async (req, resp) => {
-  try {
-    const listEntry = await ReadingList.create({
-      blogId: req.body.blogId,
-      userId: req.body.userId,
-    })
-    return resp.status(200).json(listEntry)
-  } catch (error) {
-    return resp.status(400).json({ error: error })
+router.post('/', tokenExtractor, sessionValidator, async (req, resp) => {
+  if(req.sessionValid){
+    try {
+      const listEntry = await ReadingList.create({
+        blogId: req.body.blogId,
+        userId: req.body.userId,
+      })
+      return resp.status(200).json(listEntry)
+    } catch (error) {
+      return resp.status(400).json({ error: 'Malformed request' })
+    }
+  }
+  else {
+    return resp.status(401).json ({ error: 'Please login to add to your reading list' })
   }
 })
 
